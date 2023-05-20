3e8ee8f44cbca9e04d529e8b5eaf523c92de7fe7
your_username <your_email@gmail.com>
2023-05-20 17:28:09 +1000

    failed test - empty firstname

--- src/person.py
+++ src/person.py
@@ -1,5 +1,8 @@
 class Person:
     def __init__(self,first_name=None,last_name=None):
+        if not first_name or not last_name:
+            raise ValueError("First and last names must have values")
+        
         self._first_name = first_name
         self._last_name = last_name
     
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,6 +1,9 @@
 from src.person import Person
 
 
+def test_person_initialization_nofirstname():
+    p1 = Person(first_name=None, last_name="Sanders")
+
 def test_full_name():
     p1 = Person("Adam", "Sanders")
     assert p1.get_full_name() == "Adam Sanders"
