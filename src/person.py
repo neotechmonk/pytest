f24e4d1d7049da529e5b76664a5cfb605333d83a
your_username <your_email@gmail.com>
2023-05-20 17:22:04 +1000

    implemented get_full_name()

--- src/person.py
+++ src/person.py
@@ -1,3 +1,7 @@
 class Person:
     def __init__(self,first_name=None,last_name=None):
-        pass
+        self._first_name = first_name
+        self._last_name = last_name
+    
+    def get_full_name(self):
+        return f"{self._first_name} {self._last_name}"
