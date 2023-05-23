1bcd46704fdeeca5a7f3774812d9e2b92c0a0034
your_username <your_email@gmail.com>
2023-05-23 22:39:40 +1000

    refactor to better error handle

--- src/aggregate.py
+++ src/aggregate.py
@@ -8,10 +8,13 @@ def get_age_stat(data, lastname, stat):
     for row in data:
         if row['LastName'] == lastname:
             age_list.append(row['Age'])
-    if stat.lower() == 'mean':
-        stat_result = mean(stat_result)
-    if stat.lower() == 'median':
-        stat_result = median(stat_result)
-    
+
+            
+    if age_list:   
+        if stat.lower() == 'mean':
+            stat_result = mean(age_list)
+        if stat.lower() == 'median':
+            stat_result = median(age_list)
+        
     return {'LastName':lastname,
             stat:stat_result}
