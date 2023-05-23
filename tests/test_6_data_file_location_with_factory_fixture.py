638d7cbb47cc71dd16610f0e89442edebb7413c8
your_username <your_email@gmail.com>
2023-05-23 22:01:45 +1000

    bug fix file type checking with endswith()

--- tests/test_6_data_file_location_with_factory_fixture.py
+++ tests/test_6_data_file_location_with_factory_fixture.py
@@ -15,16 +15,21 @@ def process_data(data_folder):
     def _file_type(file_name_or_type):
         for f in files:
             if file_name_or_type in f:
-                if file_name_or_type != ".json":
-                    data=data_processor.proces_csv_data(data_folder+f)
-                else:
+                if file_name_or_type.endswith(".json"):
                     data=data_processor.proces_json_data(data_folder+f)
+                else:
+                    data=data_processor.proces_csv_data(data_folder+f)
         return data
     yield _file_type
 
                 
-def test_headers(process_data):
+def test_headers_csv(process_data):
     data =  process_data(file_name_or_type="clean_list.csv")
+    header = list(data[0].keys())
+    assert header == ['FirstName', 'LastName', 'Age']
+
+def test_headers_json(process_data):
+    data =  process_data(file_name_or_type="clean_list.json")
     header = list(data[0].keys())
     assert header == ['FirstName', 'LastName', 'Age']
 
